0347a86563ff6efa5b28c2271bda8ae2c45f055e
Elfan ulil Albab <102909928+elfan03@users.noreply.github.com>
2023-10-18 02:09:54 +0900

    tes

--- app/src/main/java/com/example/fagmentnavbarhome/activity_splash.kt
+++ app/src/main/java/com/example/fagmentnavbarhome/activity_splash.kt
@@ -1,20 +1,20 @@
 package com.example.fagmentnavbarhome
 
 import android.content.Intent
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-class Splashscreen : AppCompatActivity() {
+class activity_splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splashscreen)
+        setContentView(R.layout.activity_splash)
         CoroutineScope(Dispatchers.Main).launch {
             delay(2000)
-            val intent = Intent(this@Splashscreen, MainActivity::class.java)
+            val intent = Intent(this@activity_splash, activity_Login::class.java)
             startActivity(intent)
             finish()
         }
